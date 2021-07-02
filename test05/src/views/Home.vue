84ca614d6bf39895338b08b6a9c6d62e17b073bc
Gyubi Kim <85974080+gyubikim@users.noreply.github.com>
2021-07-02 17:41:55 +0900

    naver

--- test05/src/views/Home.vue
+++ test05/src/views/Home.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="https://cdn.shopify.com/s/files/1/0231/6137/2752/files/logo_410x.jpg?v=1573431073" />
-    <HelloWorld msg="New Collections" />
-    
-  </div>
+ <img v-bind:src="logo">
+ <div><img v-bind:src="main"></div>
+ <div v-html="htmlString0"></div>
+ <div v-html="htmlString1"></div>
+  <div><img v-bind:src="pink"></div><br><div><img v-bind:src="Yellow"></div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: "",
+  components: {},
+  data() {
+    return {
+      logo: "https://cdn.shopify.com/s/files/1/0231/6137/2752/files/logo_410x.jpg?v=1573431073",
+    main:"https://i.shgcdn.com/011108a3-b3e0-4e62-9627-9b8ab27c0d00/-/format/auto/-/preview/3000x3000/-/quality/best/",
+    htmlString0:"<h2>New Collections</h2>",
+    htmlString1:"<p>Cuteness Overload</p>",
+    pink:"https://i.shgcdn.com/8cc96055-9d7e-4c90-acc3-42b2d15ca9c8/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+    yellow:"https://i.shgcdn.com/f59dc46f-2d28-4990-84a3-e7be636a925c/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+    };
   },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
 };
 </script>

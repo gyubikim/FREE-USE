ffbd26b23607f49b4882980b4d59900f0f6f1702
Gyubi Kim <85974080+gyubikim@users.noreply.github.com>
2021-07-05 09:43:24 +0900

    -

--- test05/src/views/Home.vue
+++ test05/src/views/Home.vue
@@ -1,4 +1,14 @@
 <template>
+<input type="text" v-model.number="num1">
+<input type="text" v-model="num1">
+<button @click="changeValue()">값 변경</button>
+<select v-model="city" @change="getSelectValue">
+  <option value="02">서울</option>
+  <option value="064">제주</option>
+  <option value="022">부산</option>
+</select>
+<button @click="getSelectValue">셀렉트시티버튼</button>
+<!-- <div>선택한 지역:{{selCity}}</div> -->
  <img v-bind:src="logo">
  <textarea v-model="msg"></textarea>
  <input type="text" v-model="txt1">
@@ -22,12 +32,22 @@ export default {
     yellow:"https://i.shgcdn.com/f59dc46f-2d28-4990-84a3-e7be636a925c/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
     msg:"여러 줄을 입력할 수 있는 텍스트 에리아 테스트박스당~!",
     txt1:"",
+    city:"064",
+    
+    // selCity:[],
+    getSelectValue(){
+      console.log(this.city);
+    }
     };
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    changeValue(){
+      this.txt1="dslfhsdihf";
+    }
+  },
 };
 </script>
--- test05/src/views/shop.vue
+++ test05/src/views/shop.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>
+    <label><input type="checkbox" value="서울" v-model="checked">서울</label>
+    <label><input type="checkbox" value="부산" v-model="checked">부산</label>
+    <label><input type="checkbox" value="제주" v-model="checked">제주</label>
+    <br>
+    <span>체크한 지역:{{checked}}</span>
+
+    </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      checked:[]
+    };
+  }
+}
+
+consol.log(알겠습니다);
+</script>
